3d54e6fc7fdacc1635024df55f12e4dbaf70a52e
gtiwari333 <gtiwari333@gmail.com>
2019-08-10 10:43:50 -0500

    custom error page

---
+++ src/main/java/g/t/app/web/mvc/ErrorControllerAdvice.java
@@ -0,0 +1,25 @@
+package g.t.app.web.mvc;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ControllerAdvice
+@Slf4j
+public class ErrorControllerAdvice {
+
+
+    @ExceptionHandler(Throwable.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public String exception(final Throwable throwable, final Model model) {
+        log.error("Exception during execution of application", throwable);
+
+        String errorMessage = (throwable != null ? throwable.getMessage() : "Unknown error");
+        model.addAttribute("errorMessage", errorMessage);
+        return "error";
+    }
+
+}
