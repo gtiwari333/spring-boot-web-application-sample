e6168877c372aaa09aaeb17e492c2d8c72f092b7
gtiwari333 <gtiwari333@gmail.com>
2019-08-11 17:51:10 -0500

    added ecr deploy notes, activated caching filter for prod/docker

--- docker-steps.md
+++ docker-steps.md
@@ -1,6 +1,6 @@
 Run locally
 ===
-    mvn clean compile package
+    mvn clean compile package -Pdocker
     sudo docker build -t app .
     sudo docker run -p 8080:8080 app
 
@@ -49,6 +49,23 @@ Example
  
     $ aws configure
     $ sudo aws ecr create-repository --repository-name noteapp --region us-east-1    << returned repositoryUri": "159931644654.dkr.ecr.us-east-1.amazonaws.com/noteapp
+    $ mvn clean compile package -Pdocker
     $ sudo docker build -t app .
     $ sudo docker tag app 159931644654.dkr.ecr.us-east-1.amazonaws.com/noteapp
+    $ aws ecr get-login --no-include-email --region us-east-1   << otherwise we get error: ``denied: The security token included in the request is invalid.``
     $ sudo docker push 159931644654.dkr.ecr.us-east-1.amazonaws.com/noteapp
+
+### Create and Login to EC2 instance 
+    https://docs.aws.amazon.com/AmazonECS/latest/developerguide/get-set-up-for-amazon-ecs.html
+   
+   
+Terms:
+
+- EC2 - alternative of beanstalk -- allows to create a machine and clusters
+- ECR - container registry -- we can push docker images
+- IAM - identity mgmt --> need to create a second account before creating EC2 instances/ publish image on ECR etc .. give full access
+
+
+- Need to create EC2 cluster to deploy docker image
+- Create EC2 cluster, a service, task using the docker image and run the task to deploy the image
+- A task should be created to do the deploy
---
+++ src/main/java/g/t/app/config/WebConfigurer.java
@@ -0,0 +1,66 @@
+package g.t.app.config;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.web.servlet.ServletContextInitializer;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.core.env.Profiles;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.EnumSet;
+import java.util.concurrent.TimeUnit;
+
+@Configuration
+@RequiredArgsConstructor
+@Slf4j
+public class WebConfigurer implements ServletContextInitializer {
+
+    private final Environment env;
+
+    @Override
+    public void onStartup(ServletContext servletContext) {
+
+        EnumSet<DispatcherType> disps = EnumSet.of(DispatcherType.REQUEST, DispatcherType.FORWARD, DispatcherType.ASYNC);
+
+        if (env.acceptsProfiles(Profiles.of(Constants.SPRING_PROFILE_PRODUCTION, Constants.SPRING_PROFILE_DOCKER))) {
+            initCachingHttpHeadersFilter(servletContext, disps);
+        }
+    }
+
+    private void initCachingHttpHeadersFilter(ServletContext servletContext, EnumSet<DispatcherType> disps) {
+        log.debug("Registering Caching HTTP Headers Filter");
+        FilterRegistration.Dynamic cachingHttpHeadersFilter = servletContext.addFilter("cachingHttpHeadersFilter", new CachingHttpHeadersFilter());
+
+        cachingHttpHeadersFilter.addMappingForUrlPatterns(disps, true, "/webjars/*");
+        cachingHttpHeadersFilter.addMappingForUrlPatterns(disps, true, "/static/*");
+        cachingHttpHeadersFilter.setAsyncSupported(true);
+    }
+
+    static class CachingHttpHeadersFilter extends OncePerRequestFilter {
+
+        private final long LAST_MODIFIED = System.currentTimeMillis();
+        private long cacheTimeToLive = TimeUnit.DAYS.toMillis(1461L);
+
+        @Override
+        public void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws IOException, ServletException {
+
+            response.setHeader("Cache-Control", "max-age=" + cacheTimeToLive + ", public");
+            response.setHeader("Pragma", "cache");
+
+            // Setting Expires header, for proxy caching
+            response.setDateHeader("Expires", cacheTimeToLive + System.currentTimeMillis());
+
+            // Setting the Last-Modified header, for browser caching
+            response.setDateHeader("Last-Modified", LAST_MODIFIED);
+
+            filterChain.doFilter(request, response);
+        }
+    }
+
+}
