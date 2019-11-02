a59ccc08303d4ac595e097bbe3a418335e5e9d4b
Ganesh <gtiwari333@gmail.com>
2019-11-02 16:39:21 -0500

    added tests for app context and @Transactional tests

---
+++ src/test/java/g/t/app/ApplicationTest.java
@@ -0,0 +1,18 @@
+package g.t.app;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+@Slf4j
+class ApplicationTest {
+
+    @Test
+    void contextLoads() {
+        log.info("Context loads !!!");
+    }
+
+}
+
+
