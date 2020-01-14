ddef28abcc28d42f29ba2f8a29e710b7dba4e3fd
Ganesh Tiwari <gtiwari@sdata.us>
2020-01-13 19:16:34 -0600

    jooq config

--- core/src/main/java/gt/app/web/rest/HelloResource.java
+++ core/src/main/java/gt/app/web/rest/HelloResource.java
@@ -1,14 +1,20 @@
 package gt.app.web.rest;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
 
 @RestController
+@RequiredArgsConstructor
+@RequestMapping("/public")
+@Slf4j
 public class HelloResource {
 
-    @GetMapping("/api/hello")
+    @GetMapping("/hello")
     public Map<String, String> sayHello() {
         return Map.of("hello", "world");
     }
--- core/src/main/java/gt/app/web/rest/UserResource.java
+++ core/src/main/java/gt/app/web/rest/UserResource.java
@@ -1,8 +1,10 @@
 package gt.app.web.rest;
 
 import gt.app.config.security.SecurityUtils;
+import gtapp.jooq.Tables;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.jooq.DSLContext;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,9 +15,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 @Slf4j
 public class UserResource {
+    final DSLContext dsl;
 
     @GetMapping("/account")
     public User getAccount() {
+        log.error("Hello " + dsl.fetch(Tables.AUTHORITY).size());
+
         return SecurityUtils.getCurrentUserDetails();
     }
 }
