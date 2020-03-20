7b91c42ddaabb0aea753d10b85b576b59d09821d
Ganesh <gtiwari333@gmail.com>
2020-03-19 20:03:50 -0500

    i18n

--- core/src/main/java/gt/app/config/WebMvcConfig.java
+++ core/src/main/java/gt/app/config/WebMvcConfig.java
@@ -2,9 +2,17 @@ package gt.app.config;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.autoconfigure.web.ResourceProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+import java.util.Locale;
 
 @Configuration
 @RequiredArgsConstructor
@@ -16,6 +24,24 @@ public class WebMvcConfig implements WebMvcConfigurer {
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("favicon.ico")
             .addResourceLocations(this.resourceProperties.getStaticLocations());
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(localeInterceptor());
+    }
+
+    public HandlerInterceptor localeInterceptor() {
+        LocaleChangeInterceptor localeInt = new LocaleChangeInterceptor();
+        localeInt.setParamName("lang");
+        return localeInt;
+    }
+
+    @Bean
+    public LocaleResolver localeResolver() {
+        SessionLocaleResolver slr = new SessionLocaleResolver();
+        slr.setDefaultLocale(Locale.US);
+        return slr;
     }
 
 }
