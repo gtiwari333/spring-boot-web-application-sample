65fa26dda5bef665996d3694620ea6c60e2c54ca
Ganesh Tiwari <gtiwari333@gmail.com>
2020-07-13 23:10:03 -0500

    dependency updates  with Spring Boot 2.4.0-M1

--- core/src/main/java/gt/app/config/SwaggerConfig.java
+++ core/src/main/java/gt/app/config/SwaggerConfig.java
@@ -8,10 +8,10 @@ import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import springfox.documentation.swagger2.annotations.EnableSwagger2WebMvc;
 
 @Configuration
-@EnableSwagger2
+@EnableSwagger2WebMvc
 public class SwaggerConfig {
     @Bean
     public Docket api() {
