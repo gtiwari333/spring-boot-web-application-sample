2d7b9d272e1ec1512582979719c691dbcc2d260b
Ganesh Tiwari <gtiwari333@gmail.com>
2020-08-02 18:12:04 -0500

    better module name

---
+++ trend-service/src/main/java/gt/trend/App.java
@@ -0,0 +1,36 @@
+package gt.trend;
+
+import gt.trend.config.Constants;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.Arrays;
+import java.util.Map;
+
+@SpringBootApplication
+@Slf4j
+public class App {
+
+    public static void main(String[] args) throws UnknownHostException {
+
+        SpringApplication app = new SpringApplication(App.class);
+        app.setDefaultProperties(Map.of("spring.profiles.default", Constants.SPRING_PROFILE_DEVELOPMENT));
+        Environment env = app.run(args).getEnvironment();
+
+        log.info("Access URLs:\n----------------------------------------------------------\n\t" +
+                "Local: \t\t\thttp://localhost:{}\n\t" +
+                "External: \t\thttp://{}:{}\n\t" +
+                "Environment: \t{} \n\t" +
+                "----------------------------------------------------------",
+            env.getProperty("server.port"),
+            InetAddress.getLocalHost().getHostAddress(),
+            env.getProperty("server.port"),
+            Arrays.toString(env.getActiveProfiles())
+        );
+    }
+
+}
---
+++ trend-service/src/main/java/gt/trend/config/Constants.java
@@ -0,0 +1,12 @@
+package gt.trend.config;
+
+public class Constants {
+
+    public static final String SPRING_PROFILE_DEVELOPMENT = "dev";
+    public static final String SPRING_PROFILE_PRODUCTION = "prod";
+    public static final String SPRING_PROFILE_DOCKER = "docker";
+    public static final String SPRING_PROFILE_TEST = "test";
+
+    private Constants() {
+    }
+}
