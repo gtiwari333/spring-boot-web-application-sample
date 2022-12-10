51da2448e4889343243f1c3b9685b82d133c97f8
Ganesh Tiwari <gtiwari333@gmail.com>
2022-12-10 13:04:10 -0600

    build fix

--- main-app/main-webapp/src/test/java/gt/app/frwk/BaseSeleniumTest.java
+++ main-app/main-webapp/src/test/java/gt/app/frwk/BaseSeleniumTest.java
@@ -7,7 +7,7 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.boot.test.web.server.LocalServerPort;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public abstract class BaseSeleniumTest {
