45a46b502b508c044ec6324e75437e0eb7706299
Ganesh Tiwari <gtiwari333@gmail.com>
2022-12-18 14:01:54 -0600

    email validation

--- email/email-service-api/src/main/java/gt/api/email/EmailDto.java
+++ email/email-service-api/src/main/java/gt/api/email/EmailDto.java
@@ -1,11 +1,16 @@
 package gt.api.email;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+
 import java.util.Collection;
 import java.util.List;
 
-public record EmailDto(String fromEmail, String fromName, Collection<String> to, Collection<String> cc,
+public record EmailDto(@Email String fromEmail, String fromName, Collection<String> to, Collection<String> cc,
                        Collection<String> bcc,
-                       String subject, String content, boolean isHtml, FileBArray[] files) {
+                       @NotNull @NotEmpty String subject, @NotNull @NotEmpty String content, boolean isHtml,
+                       FileBArray[] files) {
 
     public static EmailDto of(String fromEmail, Collection<String> to, String subject, String content) {
         return new EmailDto(fromEmail, fromEmail, to, List.of(), List.of(), subject, content, false, new FileBArray[]{});
