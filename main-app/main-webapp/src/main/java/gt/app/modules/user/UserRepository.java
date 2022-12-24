0ec10e9f94dc33f6bb8edf9a19c375f72cba4bd5
Ganesh Tiwari <gtiwari333@gmail.com>
2022-12-24 10:20:58 -0600

    checkstyle fix

--- main-app/main-webapp/src/main/java/gt/app/modules/user/UserRepository.java
+++ main-app/main-webapp/src/main/java/gt/app/modules/user/UserRepository.java
@@ -4,8 +4,6 @@ import gt.app.domain.AppUser;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
