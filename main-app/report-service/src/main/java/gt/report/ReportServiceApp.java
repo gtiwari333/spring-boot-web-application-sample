a67095c975b3179ecd5ff509230cb68fc85b7a1f
Ganesh T <gtiwari333@gmail.com>
2023-12-25 13:28:10 -0600

    text block

--- main-app/report-service/src/main/java/gt/report/ReportServiceApp.java
+++ main-app/report-service/src/main/java/gt/report/ReportServiceApp.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.core.env.Environment;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 import java.util.Arrays;
 
 @SpringBootApplication
@@ -13,13 +15,21 @@ import java.util.Arrays;
 @EnableScheduling
 public class ReportServiceApp {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws UnknownHostException {
 
         SpringApplication app = new SpringApplication(ReportServiceApp.class);
         Environment env = app.run(args).getEnvironment();
 
-        log.info("Environment: \t{} \n\t" +
-                "----------------------------------------------------------",
+        log.info("""
+                        Access URLs:
+                        ----------------------------------------------------------
+                        \tLocal: \t\t\thttp://localhost:{}
+                        \tExternal: \t\thttp://{}:{}
+                        \tEnvironment: \t{}\s
+                        ----------------------------------------------------------""",
+            env.getProperty("server.port"),
+            InetAddress.getLocalHost().getHostAddress(),
+            env.getProperty("server.port"),
             Arrays.toString(env.getActiveProfiles())
         );
     }
