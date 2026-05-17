a647f09c5f8b8f696757c1b0e1713fd4be8ab965
Ganesh Tiwari <gtiwari333@gmail.com>
2026-05-17 11:59:07 -0500

    Replace MappingJackson2MessageConverter with JacksonJsonMessageConverter

--- trend/trend-service/src/main/java/gt/trend/TrendServiceApp.java
+++ trend/trend-service/src/main/java/gt/trend/TrendServiceApp.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.env.Environment;
 import org.springframework.jms.annotation.JmsListener;
-import org.springframework.jms.support.converter.MappingJackson2MessageConverter;
+import org.springframework.jms.support.converter.JacksonJsonMessageConverter;
 import org.springframework.jms.support.converter.MessageConverter;
 import org.springframework.jms.support.converter.MessageType;
 
@@ -52,7 +52,7 @@ public class TrendServiceApp {
 
     @Bean
     MessageConverter jacksonJmsMessageConverter() {
-        var converter = new MappingJackson2MessageConverter();
+        var converter = new JacksonJsonMessageConverter();
         converter.setTargetType(MessageType.TEXT);
         converter.setTypeIdPropertyName("_type");
         return converter;
